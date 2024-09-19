9b5203ac69e01e09dc6e15bc98de2077df8c28f4
aibeknet <aaskarov0690.kg@gmail.com>
2024-09-19 14:10:48 +0600

    Add Task component.

---
+++ src/components/Task.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface TaskProps {
+    task: {
+        id: string;
+        text: string;
+        completed: boolean;
+    };
+    onDelete: (id: string) => void;
+    onToggle: (id: string) => void;
+}
+
+const Task: React.FC<TaskProps> = ({ task, onDelete, onToggle }) => {
+    return (
+        <div>
+            <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => onToggle(task.id)}
+            />
+            <span>{task.text}</span>
+            <button onClick={() => onDelete(task.id)}>Delete</button>
+        </div>
+
+    );
+};
+
+export default Task;
